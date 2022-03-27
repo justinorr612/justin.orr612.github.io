a4f727bef02aef98ea00a41265619b72086ea06c
justinorr612 <97361066+justinorr612@users.noreply.github.com>
2022-03-27 17:00:05 -0500

    Create main.js

---
+++ main.js
@@ -0,0 +1,12 @@
+<nav class="navbar navbar-inverse navbar-fixed-top">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <a class="navbar-brand" href="#">Justin Orr</a>
+    </div>
+    <ul class="nav navbar-nav">
+      <li class="active"><a href="#">Home</a></li>
+      <li><a href="#">Bio</a></li>
+      <li><a href="#">Projects</a></li>
+    </ul>
+  </div>
+</nav>
